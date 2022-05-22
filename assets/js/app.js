30390a4944c052fddb7ae07bb36cfa688867a80c
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-05-22 18:03:43 +0200

    \update\

---
+++ assets/js/app.js
@@ -0,0 +1,13 @@
+// function openMenu() {
+//     if ($(window).width() <= 768) {
+//         document.getElementById("menu-trigger").style.width = "60%";
+//     }
+
+// }
+
+// function closeMenu() {
+//     if ($(window).width() <= 768) {
+//         document.getElementById("menu-trigger").style.width = "0%";
+//     }
+
+// }

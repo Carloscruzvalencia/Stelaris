78a0231e5071b718b5e065f5e004aa4920e45266
Carlos cruz valencia <a2carcruzinfo@gmail.com>
2022-05-22 19:02:52 +0200

    \update\

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,9 +1,11 @@
 function openMenu() {
     document.getElementById("menu-eff").style.width = "100%";
-    document.getElementById("blur-eff").style.width = "100%";
+    document.getElementById("blur-eff").style.filter = "blur(5px)";
 
 }
 
 function closeMenu() {
     document.getElementById("menu-eff").style.width = "0%";
+    document.getElementById("blur-eff").style.filter = "blur(0px)";
+
 }
